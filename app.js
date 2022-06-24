1547c4f1e4b135b4cb73d508ab50775273fdb205
Teklehaimanot Belete <enbish.blt@gmail.com>
2022-06-24 16:10:38 +0300

    make an api to fetch all sensor data

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const data = require('./data.json')
 var cors = require('cors')
 require('dotenv/config')
 
@@ -6,10 +7,15 @@ var app = express()
 app.use(cors())
 app.use(express.json())
 
+
 app.get('/', (req, res) => {
     res.send('we are on home page')
 })
 
+app.get('/api/sensor', (req, res) => {
+    res.status(200).json(data)
+
+})
 app.listen(process.env.PORT, () => {
     console.log('server started')
 })
